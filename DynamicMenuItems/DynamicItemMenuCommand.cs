5ad0575c4e5d17b4c81c14318b9387e7d74d24ae
Somnuk Wongkhan <somnuk.wk@gmail.com>
2016-07-24 22:56:01 +0700

    Create DynamicMenuItems project

---
+++ DynamicMenuItems/DynamicItemMenuCommand.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.Shell;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicMenuItems
+{
+    public class DynamicItemMenuCommand: OleMenuCommand
+    {
+        private Predicate<int> _matches;
+
+        public DynamicItemMenuCommand(
+            CommandID rootId, 
+            Predicate<int> matches, 
+            EventHandler invokeHandler,
+            EventHandler beforeQueryStatusHandler)
+            : base(invokeHandler,null, beforeQueryStatusHandler, rootId)
+        {
+            if(matches == null)
+            {
+                throw new ArgumentException(nameof(matches));
+            }
+
+            _matches = matches;
+        }
+
+        public override bool DynamicItemMatch(int cmdId)
+        {
+            if(_matches(cmdId))
+            {
+                this.MatchedCommandId = cmdId;
+                return true;
+            }
+
+            this.MatchedCommandId = 0;
+            return false;
+        }
+
+    }
+}
---
+++ DynamicMenuItems/DynamicMenuPackage.cs
@@ -0,0 +1,48 @@
+﻿//------------------------------------------------------------------------------
+// <copyright file="DynamicMenuPackage.cs" company="Company">
+//     Copyright (c) Company.  All rights reserved.
+// </copyright>
+//------------------------------------------------------------------------------
+
+using System;
+using System.ComponentModel.Design;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.OLE.Interop;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.Win32;
+
+namespace DynamicMenuItems
+{
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    [Guid(DynamicMenuPackage.PackageGuidString)]
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
+    public sealed class DynamicMenuPackage : Package
+    {
+        public const string PackageGuidString = "c0db142d-26d7-430d-9afc-560ee947e7ed";
+
+        public DynamicMenuPackage()
+        {
+        }
+
+        #region Package Members
+
+        /// <summary>
+        /// Initialization of the package; this method is called right after the package is sited, so this is the place
+        /// where you can put all the initialization code that rely on services provided by VisualStudio.
+        /// </summary>
+        protected override void Initialize()
+        {
+            DynamicMenu.Initialize(this);
+            base.Initialize();
+        }
+
+        #endregion
+    }
+}
